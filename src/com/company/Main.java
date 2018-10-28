c8eea9f07f04ca2b0e3e59cb4b9b8d36e3704209
ferchee <ferenc.nyerges@gmail.com>
2018-10-28 20:52:06 +0100

    Typo fixed ( hellow)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hellow World!");
+        System.out.println("Hello World!");
 	// write your code here
     }
 }
